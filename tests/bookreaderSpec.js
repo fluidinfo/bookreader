3b72429deb02549a26ab66ae07d0c8ed099f7ff6
Nicholas H.Tollervey <ntoll@ntoll.org>
2011-08-05 00:38:22 +0100

    Start of a specification

--- tests/bookreaderSpec.js
+++ tests/bookreaderSpec.js
@@ -16,6 +16,33 @@ describe("bookreader.js", function() {
     this.fi = fluidinfo({username: "username", password: "password"});
   });
 
+  describe("Bookreader object", function() {
+    it("should allow a user to log in for a session", function() {
+    });
+
+    it("should allow a user to log out of a session", function() {
+    });
+
+    it("should store session details locally on the browser", function() {
+    });
+
+    it("should remember session details from the browser local storage", function() {
+    });
+
+  });
+
+  describe("Work object", function() {
+  });
+
+  describe("Friends object", function() {
+  });
+
+  describe("Block object", function() {
+  });
+
+  describe("Library object", function() {
+  });
+
   afterEach(function() {
     this.server.restore();
   });
